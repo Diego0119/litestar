6b363397b19c73764f6be883dc16acfa1546df85
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-10-13 21:59:54 -0300

    modificando a postgres

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from litestar.plugins.sqlalchemy import SQLAlchemyPlugin, SQLAlchemySyncConfig
 from app.models import Base
 
 db_config = SQLAlchemySyncConfig(
-    connection_string="sqlite:///todo.sqlite3",
+    connection_string="postgresql+psycopg2://user:password@localhost:5432/todo",
     metadata=Base.metadata,
     create_all=True,
     before_send_handler=sync_autocommit_before_send_handler,
